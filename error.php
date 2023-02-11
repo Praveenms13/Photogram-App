ebde25e9fff4a634a1d985952c10d1f1560fa083
M.S.Praveen Kumar <mspraveenkumar77@gmail.com>
2023-02-11 04:44:25 +0000

    Added_error_msg

---
+++ error.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=, initial-scale=1.0">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<title>Document</title>
+</head>
+
+<body>
+	<div class="container my-5">
+		<div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
+			<div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
+				<h1 class="display-4 fw-regular lh-1">Under Developing...</h1>
+				<p class="lead">This Part is under Maintanence, Please login or signup via Email. This part will be
+					available soon.</p>
+				<div class="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3">
+					<a type="button" class="btn btn-primary btn-lg px-4 me-md-2 fw-bold"
+						href="https://photogramapp.selfmade.one">Go
+						Back</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+
+</html>
