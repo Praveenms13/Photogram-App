e02cb2bd8997d9dde7bb51149708efd5a9e268cb
praveen <mspraveenkumar77@gmail.com>
2023-03-12 16:41:10 +0000

    fix

---
+++ libs/load.php
@@ -0,0 +1,32 @@
+<?php
+
+        include "_Includes/User.class.php";
+        include "_Includes/Database.class.php";
+        include "_Includes/Session.class.php";
+        include "_Includes/UserSession.class.php";
+        global $__DBconfig;
+        if ($_SERVER['APPLICATION_ENV'] == "Production") {
+            $__DBconfig = file_get_contents($_SERVER['DOCUMENT_ROOT'] . "/../../config_files/photogram.json");
+        } else { 
+            $__DBconfig = file_get_contents($_SERVER['DOCUMENT_ROOT'] . "/../photogramConfig.json");
+        }
+        Session::start();
+
+        function get_config($key, $default_key = 0)
+        {
+            global $__DBconfig;
+            $config = json_decode($__DBconfig, true);
+            if (isset($config[$key])) {
+                return $config[$key];
+            } else {
+                return $default_key;
+            }
+        }
+        function loadAccess($file_form)
+        {
+            include $_SERVER['DOCUMENT_ROOT'] . get_config('path') . "template/$file_form.php";
+        }
+        function loadAc($file)
+        {
+            include $_SERVER['DOCUMENT_ROOT'] . get_config('path') . "album/$file.php";
+        }
