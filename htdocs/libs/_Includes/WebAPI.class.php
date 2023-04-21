e4f0ddb80faa6338c92ea2791f65f97cfc42446d
praveen <mspraveenkumar77@gmail.com>
2023-04-21 22:34:11 +0000

    Project_Architecture

--- htdocs/libs/_Includes/WebAPI.class.php
+++ htdocs/libs/_Includes/WebAPI.class.php
@@ -6,12 +6,12 @@ class WebAPI
     {
         if (php_sapi_name() == 'cli') {
             global $__DBconfig;
-            $__DBconfigPath = "/home/mspraveenkumar77/config_files/photogram.json";
+            $__DBconfigPath = "/home/mspraveenkumar77/htdocs/photogram/WorkSpaceConfiguration/photogram.json";
             $__DBconfig = file_get_contents($__DBconfigPath);
         } elseif (php_sapi_name() == 'apache2handler') {
             global $__DBconfig;
             $__DBconfigPath = dirname(is_link($_SERVER['DOCUMENT_ROOT']) ? readlink($_SERVER['DOCUMENT_ROOT']) : $_SERVER['DOCUMENT_ROOT']);
-            $__DBconfig = file_get_contents($__DBconfigPath . "/../config_files/photogram.json");
+            $__DBconfig = file_get_contents($__DBconfigPath . "/WorkSpaceConfiguration/photogram.json");
         }
         Database::getConnection();
     }
