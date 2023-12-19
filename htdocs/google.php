afd3c0eb6f3b5b2708c23e5b8de5f2f11e925135
praveen <mspraveenkumar77@gmail.com>
2023-12-19 13:21:38 +0000

    Updates

--- htdocs/google.php
+++ htdocs/google.php
@@ -11,17 +11,18 @@ Session::loadTemplate('_head')
     <?php
     Session::loadTemplate('_header');
     ?>
-    <section class="jumbotron text-center" id="mainBanner">
-        <div class="container">
+    <section class="py-5 text-center container">
+        <div class="row py-lg-5">
+            <div class="col-lg-6 col-md-8 mx-auto">
             <h2 class="jumbotron-heading">Google Authentication will be available soon.</h2>
             <h2 class="jumbotron-heading">You can, </h2>
             <p>
                 <a href="../login.php" class="btn btn-outline-secondary my-2">Continue with Email &nbsp;<i class="fa-regular fa-envelope"></i></a>
 
             </p>
+            </div>
         </div>
-    </section>
-    <?php
+    </section>     <?php
     Session::loadTemplate('_footer');
     Session::loadTemplate('_script');
     ?>
