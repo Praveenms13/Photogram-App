00a0cf973706631a576e1f719853a24205abb7fe
praveen <mspraveenkumar77@gmail.com>
2024-03-02 13:39:40 +0000

    Minor Updates

--- htdocs/template/index/photogram.php
+++ htdocs/template/index/photogram.php
@@ -9,7 +9,7 @@
                 <textarea name="post_text" id="post_text" placeholder="What are you upto ?" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                 <input type="file" name="post_image" id="post_image" class="form-control form-control-lg" accept="image/*">
                 <hr class="my-3">
-                <input name="submit" id="share-memory" value="Share Memory" class="btn btn-success">
+                <button type="button" class="btn btn-success" name="submit" id="share-memory" value="Share Memory" >Share Memory</button>
             </form>
         </div>
     </div>
